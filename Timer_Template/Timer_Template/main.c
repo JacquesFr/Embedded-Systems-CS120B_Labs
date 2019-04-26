a370a6315d0b066c4dffcfff09b81d947da7b188
JacquesFr <43708007+JacquesFr@users.noreply.github.com>
2019-04-26 13:04:33 -0700

    created timing templates

---
+++ Timer_Template/Timer_Template/main.c
@@ -0,0 +1,23 @@
+/*
+ * Timer_Template.c
+ *
+ * Created: 4/26/2019 12:57:37 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+#include "io.c"
+
+int main(void)
+{
+	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
+	
+	// Initializes the LCD display
+	LCD_init();
+	
+	// Starting at position 1 on the LCD screen, writes Hello World
+	LCD_DisplayString(1, "Hello World");
+	
+	while(1) {continue;}
+}
