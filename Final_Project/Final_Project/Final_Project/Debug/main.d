d41eaf89b25b3012eb4ff2ff750b4f246ec95833
JacquesFr <jfrac001@ucr.edu>
2019-05-30 21:28:45 -0700

    level one complete

--- Final_Project/Final_Project/Final_Project/Debug/main.d
+++ Final_Project/Final_Project/Final_Project/Debug/main.d
@@ -11,13 +11,13 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- ../../../includes/bit.h ../../../includes/timer.h \
- ../../../includes/scheduler.h \
+ ../../../includes/sys/bit.h ../../../includes/sys/timer.h \
+ ../../../includes/sys/scheduler.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- ../../../includes/io.c ../../../includes/io.h \
- ../../../includes/levelOne.h \
+ ../../../includes/sys/io.c ../../../includes/sys/io.h \
+ ../../../includes/levels/levelOne.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
@@ -50,11 +50,11 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
-../../../includes/bit.h:
+../../../includes/sys/bit.h:
 
-../../../includes/timer.h:
+../../../includes/sys/timer.h:
 
-../../../includes/scheduler.h:
+../../../includes/sys/scheduler.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
@@ -62,11 +62,11 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
-../../../includes/io.c:
+../../../includes/sys/io.c:
 
-../../../includes/io.h:
+../../../includes/sys/io.h:
 
-../../../includes/levelOne.h:
+../../../includes/levels/levelOne.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
